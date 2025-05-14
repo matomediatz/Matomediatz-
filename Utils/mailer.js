43a26908f757ddb756ba09ced4362b261146b20a
matomediatz <matomediatz@gmail.com>
2025-05-14 05:07:33 +0300

    Update mailer.js

--- Utils/mailer.js
+++ Utils/mailer.js
@@ -1,23 +1,28 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+const sendTestMail = async () => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: 'wekaemailyako@gmail.com', // badilisha hii na email yako
+      subject: 'Test Email from Matomediatz',
+      text: 'Hello! This is a test email.',
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+  } catch (err) {
+    console.error('Error sending mail:', err);
   }
-});
-
-const sendVerificationEmail = async (to, token) => {
-  const verificationLink = `http://localhost:5000/api/auth/verify/${token}`;
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to,
-    subject: 'Verify your Email - Matomediatz',
-    html: `<p>Click the link below to verify your email:</p><a href="${verificationLink}">${verificationLink}</a>`
-  };
-
-  await transporter.sendMail(mailOptions);
 };
 
-module.exports = { sendVerificationEmail };
+sendTestMail();
